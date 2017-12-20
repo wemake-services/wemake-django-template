c954271bfc66e8b87a93a43e869903125d582048
sobolevn <mail@sobolevn.me>
2017-12-20 12:30:26 +0300

    Merges Pipfile.lock

--- {{cookiecutter.project_name}}/docker/ci.sh
+++ {{cookiecutter.project_name}}/docker/ci.sh
@@ -3,12 +3,20 @@
 set -o errexit
 set -o nounset
 
+# Fail CI if `DJANGO_ENV` is set to `production`:
+if [[ "$DJANGO_ENV" == 'production' ]]; then
+  echo 'DJANGO_ENV is set to production. Running tests is not safe.'
+  exit 1
+fi
+
+# Initializing global variables and functions:
+
 : ${INSIDE_CI:=0}
 VIRTUAL_ENV_DISABLE_PROMPT=true
 
 pyclean () {
   # Cleaning cache:
-  find . | grep -E "(__pycache__|\.py[cod]$)" | xargs rm -rf
+  find . | grep -E '(__pycache__|\.py[cod]$)' | xargs rm -rf
 }
 
 # Installing dependencies:
@@ -28,32 +36,32 @@ pyclean
 python -m mypy server
 python -m pytest
 
-# Running additional checks:
+# Running code-quality check:
 xenon --max-absolute B --max-modules A --max-average A .
+
+# Checking if all the dependencies are secure and do not have any
+# known vulnerabilities:
 pipenv check
 
 # Run this part only if truly inside the CI process:
 if [[ "$INSIDE_CI" -eq 1 ]]; then
-  # Running conditional commit lint:
-  printf "$(git log -1 --pretty=%B)" | python -m gitlint.cli
-
   # Generating reports as build artifacts, it will be possible
   # to browse them later:
   # https://docs.gitlab.com/ce/user/project/pipelines/job_artifacts.html
-  mkdir -p "artifacts"
+  mkdir -p 'artifacts'
 
   # Generating pylint report (it will have issues!):
-  PYLINT=$(find . -iname "*.py" | xargs pylint --reports=y || true)
-  echo "$PYLINT" > "artifacts/pylint.rst"
+  PYLINT=$(find . -iname '*.py' | xargs pylint --reports=y || true)
+  echo "$PYLINT" > 'artifacts/pylint.rst'
 
   # Generating code-quality report:
-  radon mi . > "artifacts/mi.txt"
+  radon mi . > 'artifacts/mi.txt'
 
   # Generating complexity report:
-  radon cc . --show-closures --total-average > "artifacts/cc.txt"
+  radon cc . --show-closures --total-average > 'artifacts/cc.txt'
 
   # Generating raw metrics:
-  radon raw . > "artifacts/raw.txt"
+  radon raw . > 'artifacts/raw.txt'
 fi
 
 # Clean everything up:
