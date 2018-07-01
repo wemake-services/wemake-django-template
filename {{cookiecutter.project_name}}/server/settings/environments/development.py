5e7a77e335d647eaf209db5050284bc13f3200d1
sobolevn <mail@sobolevn.me>
2018-07-01 17:03:29 +0300

    Fixes logging for n+1

--- {{cookiecutter.project_name}}/server/settings/environments/development.py
+++ {{cookiecutter.project_name}}/server/settings/environments/development.py
@@ -6,6 +6,7 @@ This file contains all the settings that defines the development server.
 SECURITY WARNING: don't run with debug turned on in production!
 """
 
+import logging
 from typing import List
 
 from server.settings.components.common import INSTALLED_APPS, MIDDLEWARE
