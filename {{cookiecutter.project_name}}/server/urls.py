44f539651f53c1d7f49762c635187e49240b6731
proha <proha345@gmail.com>
2018-07-12 16:03:28 +0300

    "no cover"

--- {{cookiecutter.project_name}}/server/urls.py
+++ {{cookiecutter.project_name}}/server/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     url(r'^$', index, name='index'),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG: # pragma: no cover
     import debug_toolbar
     from django.views.static import serve
 
