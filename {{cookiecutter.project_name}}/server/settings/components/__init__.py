77ad44d5bd6774e076b99b0d83984797b6783c2b
sobolevn <mail@sobolevn.me>
2018-11-17 13:58:18 +0300

    Addresses review comments

--- {{cookiecutter.project_name}}/server/settings/components/__init__.py
+++ {{cookiecutter.project_name}}/server/settings/components/__init__.py
@@ -7,7 +7,7 @@ from decouple import AutoConfig
 
 # Build paths inside the project like this: BASE_DIR.joinpath('some')
 # `pathlib` is better than writing: dirname(dirname(dirname(__file__)))
-BASE_DIR = PurePath(__file__).parent.parent.parent.parent
+BASE_DIR = PurePath(__file__) - 4
 
 # Loading `.env` files
 # See docs: https://gitlab.com/mkleehammer/autoconfig
