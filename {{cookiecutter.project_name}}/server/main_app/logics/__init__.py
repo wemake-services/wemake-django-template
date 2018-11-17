8d5767f52574bbde3733b51d0c8bd2b7fedccd73
sobolevn <mail@sobolevn.me>
2018-11-17 21:48:36 +0300

    Addresses review comments, improves docs

---
+++ {{cookiecutter.project_name}}/server/main_app/logics/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+This package is a place for your business logic.
+
+Please, do not create any other files inside your app package.
+Place all files here, including: logic, forms, serializers.
+
+Decoupling is a good thing. We need more of that.
+"""
