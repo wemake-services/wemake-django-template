0b0637258b8035ba9d49ee0500945528bff8ca89
sobolevn <mail@sobolevn.me>
2018-11-18 19:18:30 +0300

    Adds architecture docs

--- {{cookiecutter.project_name}}/docker/django/gunicorn.sh
+++ {{cookiecutter.project_name}}/docker/django/gunicorn.sh
@@ -3,6 +3,9 @@
 set -o errexit
 set -o nounset
 
+# We are using `gunicorn` for production, see:
+# http://docs.gunicorn.org/en/stable/configure.html
+
 # Check that $DJANGO_ENV is set to "production",
 # fail otherwise, since it may break things:
 echo "ENV is $DJANGO_ENV"
@@ -16,7 +19,7 @@ export DJANGO_ENV
 
 # Run python specific scripts:
 # Running migrations in startup script might not be the best option, see:
-# docs/_pages/template/going-to-production.rst
+# docs/pages/template/production-checklist.rst
 python /code/manage.py migrate --noinput
 python /code/manage.py collectstatic --noinput
 python /code/manage.py compilemessages
