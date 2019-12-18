72b29005a048b7a8481e0238a97d00f1f03d1448
Nikita Sobolev <mail@sobolevn.me>
2019-12-18 19:58:58 +0300

    Closes #1023

--- {{cookiecutter.project_name}}/server/settings/environments/production.py
+++ {{cookiecutter.project_name}}/server/settings/environments/production.py
@@ -68,6 +68,10 @@ SECURE_HSTS_PRELOAD = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
+SECURE_REDIRECT_EXEMPT = [
+    # This is required for healthcheck to work:
+    '^health/',
+]
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
