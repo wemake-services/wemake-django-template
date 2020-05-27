252d6c1eecb157ee34abd0d27f72cdc9ca84e661
Kirill Malev <playittodeath@gmail.com>
2020-05-27 16:14:09 +0300

    Various improvements (#1066)
    * Closes #692, adds 0.0.0.0 and 127.0.0.1 to development settings
    
    * Fixed as discussion of PR suggests
    
    * fixed forgotten config import
    
    * Added admin to NPlusOne whitelist settings
    
    * Fixes CI
    
    * Fixes CI
    
    * Split index.html into index.html and base.html; added module comment; removed unnecessary comments.
    
    Co-authored-by: Nikita Sobolev <mail@sobolevn.me>

--- {{cookiecutter.project_name}}/server/apps/main/urls.py
+++ {{cookiecutter.project_name}}/server/apps/main/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 
 from server.apps.main.views import index
 
-# Place your URLs here:
-
 app_name = 'main'
 
 urlpatterns = [
--- {{cookiecutter.project_name}}/server/apps/main/views.py
+++ {{cookiecutter.project_name}}/server/apps/main/views.py
@@ -2,8 +2,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request: HttpRequest) -> HttpResponse:
     """
