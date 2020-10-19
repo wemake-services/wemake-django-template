50a55869a0108d101be87ca5ea4d6c7e3cdf1058
sobolevn <mail@sobolevn.me>
2020-10-19 16:53:00 +0300

    Fixes poetry

--- {{cookiecutter.project_name}}/docker/ci.sh
+++ {{cookiecutter.project_name}}/docker/ci.sh
@@ -18,7 +18,7 @@ fi
 pyclean () {
   # Cleaning cache:
   find . \
-  | grep -E '(__pycache__|\.perm|\.cache|\.static|\.py[cod]$)' \
+  | grep -E '(__pycache__|\.hypothesis|\.perm|\.cache|\.static|\.py[cod]$)' \
   | xargs rm -rf
 }
 
