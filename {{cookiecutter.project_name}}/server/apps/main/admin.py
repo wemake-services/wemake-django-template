707bd42808162e33b8c2a23aa632f6f177baf58e
sobolevn <mail@sobolevn.me>
2020-10-30 23:45:38 +0300

    Fixes CI

--- {{cookiecutter.project_name}}/server/apps/main/admin.py
+++ {{cookiecutter.project_name}}/server/apps/main/admin.py
@@ -4,5 +4,5 @@ from server.apps.main.models import BlogPost
 
 
 @admin.register(BlogPost)
-class BlogPostAdmin(admin.ModelAdmin):
+class BlogPostAdmin(admin.ModelAdmin):  # type: ignore
     """Admin panel example for ``BlogPost`` model."""
