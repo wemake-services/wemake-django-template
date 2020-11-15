43d9b90ccaa42ce5991630f3239aff5ed29d7243
Nikita Sobolev <mail@sobolevn.me>
2020-11-15 12:19:53 +0300

    Let's try without admin

--- {{cookiecutter.project_name}}/server/apps/main/admin.py
+++ {{cookiecutter.project_name}}/server/apps/main/admin.py
@@ -1,10 +1,6 @@
-import django_stubs_ext
 from django.contrib import admin
 
 from server.apps.main.models import BlogPost
-
-django_stubs_ext.monkeypatch()
-
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):  # type: ignore
