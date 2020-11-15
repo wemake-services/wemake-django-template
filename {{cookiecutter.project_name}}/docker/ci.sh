6bb285218391b1a71e2fff6428ae18d9b7d34b29
sobolevn <mail@sobolevn.me>
2020-11-15 20:58:32 +0300

    Different message 14

--- {{cookiecutter.project_name}}/docker/ci.sh
+++ {{cookiecutter.project_name}}/docker/ci.sh
@@ -15,7 +15,7 @@ fi
 pyclean () {
   # Cleaning cache:
   find . \
-  | grep -E '(__pycache__|\.hypothesis|\.perm|\.cache|\.static|\.py[cod]$)' \
+  | grep -E '(__pycache__|\.hypothesis|\.perm|\.static|\.py[cod]$)' \
   | xargs rm -rf
 }
 
