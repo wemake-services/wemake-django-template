946f635b456312fc98753bddf5b75b7ccdda0cb9
sobolevn <mail@sobolevn.me>
2020-11-15 18:46:02 +0300

    Different message 2

--- tests/test_gitlab.sh
+++ tests/test_gitlab.sh
@@ -6,7 +6,7 @@ set -o nounset
 # This file is used to setup fake project,
 # run tests inside it,
 # and remove this project completely.
-source "$CI_PROJECT_DIR/tests/build.sh"
+source './tests/build.sh'
 
 # Building the template:
 run_cookiecutter_build "$CI_PROJECT_DIR"
