9db9492782ec0806e960fa1a638cc614f12c5a92
Nikita Sobolev <mail@sobolevn.me>
2020-11-15 12:20:07 +0300

    Update admin.py

--- {{cookiecutter.project_name}}/server/apps/main/admin.py
+++ {{cookiecutter.project_name}}/server/apps/main/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from server.apps.main.models import BlogPost
 
+
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):  # type: ignore
     """Admin panel example for ``BlogPost`` model."""
