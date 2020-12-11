a139555a988dc69b9b225dd23e1ce308562dc79b
Nikita Sobolev <mail@sobolevn.me>
2020-12-11 15:09:16 +0300

    Update test_urls.py

--- {{cookiecutter.project_name}}/tests/test_server/test_urls.py
+++ {{cookiecutter.project_name}}/tests/test_server/test_urls.py
@@ -2,19 +2,19 @@ import pytest
 from django.test import Client
 
 
-def test_admin_unauthorized(client: Client) -> None:
-    """This test ensures that admin panel requires auth."""
-    response = client.get('/admin/')
-
-    assert response.status_code == 302
-
-
 @pytest.mark.django_db
 def test_health_check(client: Client) -> None:
     """This test ensures that health check is accessible."""
     response = client.get('/health/')
 
     assert response.status_code == 200
+
+
+def test_admin_unauthorized(client: Client) -> None:
+    """This test ensures that admin panel requires auth."""
+    response = client.get('/admin/')
+
+    assert response.status_code == 302
 
 
 def test_admin_authorized(admin_client: Client) -> None:
