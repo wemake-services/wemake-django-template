3c88c628da215349ff4b328fe53bc0da59413aef
Dmytro Litvinov <me@dmytrolitvinov.com>
2021-05-27 09:01:07 +0300

    Update module name for PostgreSQL

--- {{cookiecutter.project_name}}/server/settings/components/common.py
+++ {{cookiecutter.project_name}}/server/settings/components/common.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('POSTGRES_DB'),
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
