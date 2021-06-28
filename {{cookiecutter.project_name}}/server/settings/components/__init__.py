25c29b77c3249c10824b42898f1613b3e56f161f
Damien Allen <contact@dallen.co>
2021-06-28 14:52:12 +0200

    Use Path instead of PurePath in Django settings (#1643)

--- {{cookiecutter.project_name}}/server/settings/components/__init__.py
+++ {{cookiecutter.project_name}}/server/settings/components/__init__.py
@@ -1,10 +1,10 @@
-from pathlib import PurePath
+from pathlib import Path
 
 from decouple import AutoConfig
 
 # Build paths inside the project like this: BASE_DIR.joinpath('some')
 # `pathlib` is better than writing: dirname(dirname(dirname(__file__)))
-BASE_DIR = PurePath(__file__).parent.parent.parent.parent
+BASE_DIR = Path(__file__).parent.parent.parent.parent
 
 # Loading `.env` files
 # See docs: https://gitlab.com/mkleehammer/autoconfig
