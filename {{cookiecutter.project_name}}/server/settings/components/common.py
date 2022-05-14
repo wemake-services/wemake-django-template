cb1972807ba39f4e3323da4bdebf38ce3aaff753
monosans <hsyqixco@protonmail.com>
2022-05-14 16:43:36 +0300

    Set `PASSWORD_HASHERS` to the value from docs

--- {{cookiecutter.project_name}}/server/settings/components/common.py
+++ {{cookiecutter.project_name}}/server/settings/components/common.py
@@ -171,10 +171,10 @@ AUTHENTICATION_BACKENDS = (
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.ScryptPasswordHasher',
 ]
 
 
