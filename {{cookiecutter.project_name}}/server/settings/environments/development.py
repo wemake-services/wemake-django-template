edbf1c2a55f8a02570b7e8ffb4eeba2839cd29b2
Nikita Sobolev <mail@sobolevn.me>
2022-05-15 10:34:30 +0300

    Update {{cookiecutter.project_name}}/server/settings/environments/development.py

--- {{cookiecutter.project_name}}/server/settings/environments/development.py
+++ {{cookiecutter.project_name}}/server/settings/environments/development.py
@@ -71,7 +71,7 @@ MIDDLEWARE += (
 
 # https://django-debug-toolbar.readthedocs.io/en/stable/installation.html#configure-internal-ips
 INTERNAL_IPS = [
-    '{0}.1'.format(ip[: ip.rfind('.')])
+    '{0}.1'.format(ip[:ip.rfind('.')])
     for ip in socket.gethostbyname_ex(socket.gethostname())[2]
 ] + ['127.0.0.1', '10.0.2.2']
 
