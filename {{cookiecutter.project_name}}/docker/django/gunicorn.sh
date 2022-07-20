8df082d9bc224e363c9ca3fc38fe3b76822bf520
Almaz <hsyqixco@protonmail.com>
2022-07-20 13:54:07 +0000

    Precompress static files with Brotli and gzip (#2038)

--- {{cookiecutter.project_name}}/docker/django/gunicorn.sh
+++ {{cookiecutter.project_name}}/docker/django/gunicorn.sh
@@ -22,8 +22,15 @@ export DJANGO_ENV
 # Running migrations in startup script might not be the best option, see:
 # docs/pages/template/production-checklist.rst
 python /code/manage.py migrate --noinput
-python /code/manage.py collectstatic --noinput
+python /code/manage.py collectstatic --noinput --clear
 python /code/manage.py compilemessages
+
+# Precompress static files with brotli and gzip.
+# The list of ignored file types was taken from https://github.com/evansd/whitenoise
+find /var/www/django/static -type f \
+  ! -regex '^.+\.\(jpg\|jpeg\|png\|gif\|webp\|zip\|gz\|tgz\|bz2\|tbz\|xz\|br\|swf\|flv\|woff\|woff2\)$' \
+  -exec brotli --force --best {} \+ \
+  -exec gzip --force --keep --best {} \+
 
 # Start gunicorn:
 # Docs: http://docs.gunicorn.org/en/stable/settings.html
