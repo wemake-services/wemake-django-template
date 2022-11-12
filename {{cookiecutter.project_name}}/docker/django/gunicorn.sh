50d1692578e321b2efb97e9a4ca7449ba6ce3eea
monosans <hsyqixco@protonmail.com>
2022-11-12 10:21:45 +0300

    Add more extensions to precompression ignore list

--- {{cookiecutter.project_name}}/docker/django/gunicorn.sh
+++ {{cookiecutter.project_name}}/docker/django/gunicorn.sh
@@ -28,7 +28,7 @@ python /code/manage.py compilemessages
 # Precompress static files with brotli and gzip.
 # The list of ignored file types was taken from https://github.com/evansd/whitenoise
 find /var/www/django/static -type f \
-  ! -regex '^.+\.\(jpg\|jpeg\|png\|gif\|webp\|zip\|gz\|tgz\|bz2\|tbz\|xz\|br\|swf\|flv\|woff\|woff2\)$' \
+  ! -regex '^.+\.\(jpg\|jpeg\|png\|gif\|webp\|zip\|gz\|tgz\|bz2\|tbz\|xz\|br\|swf\|flv\|woff\|woff2\|3gp\|3gpp\|asf\|avi\|m4v\|mov\|mp4\|mpeg\|mpg\|webm\|wmv\)$' \
   -exec brotli --force --best {} \+ \
   -exec gzip --force --keep --best {} \+
 
