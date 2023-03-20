2960c78b493b194b347f588e4c7e32dee6380e8d
Nikita Sobolev <mail@sobolevn.me>
2023-03-21 01:48:21 +0300

    Update ci.sh

--- {{cookiecutter.project_name}}/docker/caddy/ci.sh
+++ {{cookiecutter.project_name}}/docker/caddy/ci.sh
@@ -2,19 +2,20 @@
 
 set -o errexit
 set -o nounset
-CADDYFILE_PATH="/etc/caddy/Caddyfile"
+
+CADDYFILE_PATH='/etc/caddy/Caddyfile'
 
 run_ci () {
   # Validating:
-  caddy validate --config $CADDYFILE_PATH
+  caddy validate --config "$CADDYFILE_PATH"
 
   # Checking formatting:
   # TODO: we use this hack, because `caddy fmt` does not have `--check` arg.
-  old_caddyfile="$(md5sum $CADDYFILE_PATH)"
+  old_caddyfile="$(md5sum "$CADDYFILE_PATH")"
 
-  caddy fmt --overwrite $CADDYFILE_PATH
+  caddy fmt --overwrite "$CADDYFILE_PATH"
  
-  if [ "$old_caddyfile" != "$(md5sum $CADDYFILE_PATH)" ]; then
+  if [ "$old_caddyfile" != "$(md5sum "$CADDYFILE_PATH")" ]; then
     echo 'Invalid format'
     exit 1
   else
