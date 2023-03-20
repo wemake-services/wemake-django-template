eedf670c2428390a776a4ba95e0266bcc1ffad6c
Kamil <tshibe95@gmail.com>
2023-03-20 23:46:41 +0100

    Update docker-compose.prod.yaml (#2140)
    * Update docker-compose.prod.yaml
    
    Update to deployapp with caddy
    
    * Update ci.sh
    
    * Update ci.sh
    
    maybe someone will fix that file should work with that
    
    * Update ci.sh with variable path

--- {{cookiecutter.project_name}}/docker/caddy/ci.sh
+++ {{cookiecutter.project_name}}/docker/caddy/ci.sh
@@ -2,19 +2,19 @@
 
 set -o errexit
 set -o nounset
-
+CADDYFILE_PATH="/etc/caddy/Caddyfile"
 
 run_ci () {
   # Validating:
-  caddy validate
+  caddy validate --config $CADDYFILE_PATH
 
   # Checking formatting:
   # TODO: we use this hack, because `caddy fmt` does not have `--check` arg.
-  old_caddyfile="$(md5sum /srv/Caddyfile)"
+  old_caddyfile="$(md5sum $CADDYFILE_PATH)"
 
-  caddy fmt --overwrite
-
-  if [ "$old_caddyfile" != "$(md5sum /srv/Caddyfile)" ]; then
+  caddy fmt --overwrite $CADDYFILE_PATH
+ 
+  if [ "$old_caddyfile" != "$(md5sum $CADDYFILE_PATH)" ]; then
     echo 'Invalid format'
     exit 1
   else
