c8fcd30559242d01761db13560be0159e997fc0b
Kamil <tshibe95@gmail.com>
2023-03-20 18:50:09 +0100

    Update ci.sh
    maybe someone will fix that file should work with that

--- {{cookiecutter.project_name}}/docker/caddy/ci.sh
+++ {{cookiecutter.project_name}}/docker/caddy/ci.sh
@@ -6,14 +6,14 @@ set -o nounset
 
 run_ci () {
   # Validating:
-  caddy validate
+  caddy validate --config /etc/caddy/Caddyfile
 
   # Checking formatting:
   # TODO: we use this hack, because `caddy fmt` does not have `--check` arg.
   old_caddyfile="$(md5sum /etc/caddy/Caddyfile)"
 
-  caddy fmt --overwrite
-
+  caddy fmt --overwrite /etc/caddy/Caddyfile
+ 
   if [ "$old_caddyfile" != "$(md5sum /etc/caddy/Caddyfile)" ]; then
     echo 'Invalid format'
     exit 1
