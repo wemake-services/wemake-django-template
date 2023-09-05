f2772c2161511bc1cd42cc152b0a7732ce3e7cc0
sobolevn <mail@sobolevn.me>
2023-09-05 10:20:55 +0300

    Fix tests

--- tests/test_github.sh
+++ tests/test_github.sh
@@ -17,24 +17,24 @@ cd "$PROJECT_PATH"
 export DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1
 
 # Run tests that are located inside the generate project:
-docker compose -f docker compose.yml \
-  -f docker/docker compose.prod.yml config --quiet
+docker compose -f docker-compose.yml \
+  -f docker/docker-compose.prod.yml config --quiet
 
 # Building and testing dev image:
 docker compose build --build-arg BUILDKIT_INLINE_CACHE=1
 docker compose run --user=root --rm web ./docker/django/ci.sh
 
 # Building and testing prod image:
-docker compose -f docker compose.yml \
-  -f docker/docker compose.prod.yml build
-docker compose -f docker compose.yml \
-  -f docker/docker compose.prod.yml run \
+docker compose -f docker-compose.yml \
+  -f docker/docker-compose.prod.yml build
+docker compose -f docker-compose.yml \
+  -f docker/docker-compose.prod.yml run \
   --user=root --rm web \
   python manage.py check --deploy --fail-level WARNING
 
 # Testing caddy configuration:
-docker compose -f docker compose.yml \
-  -f docker/docker compose.prod.yml run \
+docker compose -f docker-compose.yml \
+  -f docker/docker-compose.prod.yml run \
   --rm caddy sh /etc/ci.sh
 
 # Checking the size of final images:
