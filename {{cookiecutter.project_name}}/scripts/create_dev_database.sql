1574b859e8f8be51cb3190e570e7c66687be0468
Nikita Sobolev <mail@sobolevn.me>
2023-09-11 14:22:14 +0300

    Update create_dev_database.sql

--- {{cookiecutter.project_name}}/scripts/create_dev_database.sql
+++ {{cookiecutter.project_name}}/scripts/create_dev_database.sql
@@ -4,5 +4,5 @@ This file is used to bootstrap development database locally.
 Note: ONLY development database;
 */
 
-CREATE USER {{ cookiecutter.project_name }} SUPERUSER;
-CREATE DATABASE {{ cookiecutter.project_name }} OWNER {{ cookiecutter.project_name }} ENCODING 'utf-8';
+CREATE USER "{{ cookiecutter.project_name }}" SUPERUSER;
+CREATE DATABASE "{{ cookiecutter.project_name }}" OWNER "{{ cookiecutter.project_name }}" ENCODING 'utf-8';
