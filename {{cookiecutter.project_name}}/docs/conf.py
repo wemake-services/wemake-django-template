76698370dc6578be6b021b38a1d2416054ccd06b
Nikita Sobolev <mail@sobolevn.me>
2023-10-07 09:56:19 +0300

    Update conf.py (#2358)
    * Update conf.py
    
    * Update conf.py

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -30,18 +30,18 @@ django.setup()
 
 # -- Project information -----------------------------------------------------
 
-def _get_project_meta():
+def _get_project_meta() -> dict[str, str]:  # lying abour return type
     with open('../pyproject.toml', mode='rb') as pyproject:
         return tomli.load(pyproject)['tool']['poetry']
 
 
 pkg_meta = _get_project_meta()
-project = str(pkg_meta['name'])
-author = str(pkg_meta['authors'][0])
+project = pkg_meta['name']
+author = pkg_meta['authors'][0]
 copyright = author  # noqa: WPS125
 
 # The short X.Y version
-version = str(pkg_meta['version'])
+version = pkg_meta['version']
 # The full version, including alpha/beta/rc tags
 release = version
 
@@ -49,7 +49,7 @@ release = version
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '5.0'
+needs_sphinx = '7.2'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -102,11 +102,6 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'alabaster'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
