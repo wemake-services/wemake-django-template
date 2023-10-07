73966d9c48b8406385a1d5d34f54d29ee0f1d21a
Nikita Sobolev <mail@sobolevn.me>
2023-10-07 09:49:01 +0300

    Update conf.py

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -103,11 +103,6 @@ todo_include_todos = True
 # a list of builtin themes.
 html_theme = 'alabaster'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
