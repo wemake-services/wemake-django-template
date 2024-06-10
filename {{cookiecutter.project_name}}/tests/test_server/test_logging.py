4906b331bfdd6fd355c022ef6bbcaec646a581d9
Nikita Sobolev <mail@sobolevn.me>
2024-06-10 15:28:58 +0300

    Test logger (#2576)

---
+++ {{cookiecutter.project_name}}/tests/test_server/test_logging.py
@@ -0,0 +1,36 @@
+import logging
+import re
+from typing import Final
+
+import pytest
+
+_LOGGING_FORMAT_RE: Final = re.compile(
+    "timestamp='.+' level='error' event='Test message' logger='django'",
+)
+
+
+@pytest.fixture(name='logger')
+def logger_fixture() -> logging.Logger:
+    """Returns the current logger instance."""
+    return logging.getLogger('django')
+
+
+@pytest.fixture(autouse=True)
+def _redact_caplog_handlers(
+    caplog: pytest.LogCaptureFixture,
+    logger: logging.Logger,
+) -> None:
+    """Pytest inserts custom formatter, we need to reset it back."""
+    caplog.handler.setFormatter(logger.handlers[0].formatter)
+
+
+def test_logging_format(
+    caplog: pytest.LogCaptureFixture,
+    logger: logging.Logger,
+) -> None:
+    """This test ensures logging is done correctly."""
+    message = 'Test message'
+
+    logger.error(message)
+
+    assert _LOGGING_FORMAT_RE.match(caplog.text)
