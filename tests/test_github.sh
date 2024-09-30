5108912d12044d257091da41a48f3fd9e663d2c0
sobolevn <mail@sobolevn.me>
2024-09-30 08:41:12 +0300

    Add `--max-layers` to `disl` in tests

--- tests/test_github.sh
+++ tests/test_github.sh
@@ -38,5 +38,6 @@ docker compose -f docker-compose.yml \
   --rm caddy sh /etc/ci.sh
 
 # Checking the size of final images:
-disl "${PROJECT_NAME}:dev" 950MiB
-disl "registry.gitlab.com/${PROJECT_ORGANIZATION}/${PROJECT_NAME}:latest" 700MiB
+disl --max-layers=9 "${PROJECT_NAME}:dev" 950MiB
+disl --max-layers=10 \
+  "registry.gitlab.com/${PROJECT_ORGANIZATION}/${PROJECT_NAME}:latest" 700MiB
