63b510afadcd92554745de012b39fda1eac09f5a
sobolevn <mail@sobolevn.me>
2025-01-14 19:26:06 +0300

    Build docs (#2770)

---
+++ tests/build_docs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# This file is used to setup fake project,
+# run tests inside it,
+# and remove this project completely.
+. './tests/build.sh'
+
+# Building the template:
+run_cookiecutter_build "$GITHUB_WORKSPACE"
+cd "$PROJECT_PATH"
+
+# Install deps from the project:
+poetry sync --only=main --only=docs
+
+# Build docs:
+make -C docs html
+
+# Move the result docs to the dest:
+ls docs/_build
+mv docs/_build ../..
