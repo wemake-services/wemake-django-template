4b4b428364819166d5b0d2324584c34c733388cf
Dmytro Litvinov <me@dmytrolitvinov.com>
2025-02-26 13:01:18 +0200

    refactor(gunicorn): Remove deprecated and log access logs to stdout (#2779)

--- {{cookiecutter.project_name}}/docker/django/gunicorn_config.py
+++ {{cookiecutter.project_name}}/docker/django/gunicorn_config.py
@@ -12,6 +12,6 @@ workers = multiprocessing.cpu_count() * 2 + 1
 max_requests = 2000
 max_requests_jitter = 400
 
-log_file = '-'
+accesslog = '-'
 chdir = '/code'
 worker_tmp_dir = '/dev/shm'  # noqa: S108
