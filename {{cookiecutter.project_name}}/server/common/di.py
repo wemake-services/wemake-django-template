f70e186f30dd361640333869adf60cb251ca2da5
sobolevn <mail@sobolevn.me>
2026-04-05 14:53:20 +0300

    Simplify DI implementation

--- {{cookiecutter.project_name}}/server/common/di.py
+++ {{cookiecutter.project_name}}/server/common/di.py
@@ -1,28 +1,25 @@
-import importlib
-import types
-from typing import ClassVar, Protocol, final
+from typing import Any, final
 
+import punq
 
-class Resolve(Protocol):
-    """Resolve protocol for regular types."""
-
-    def __call__[Thing](self, thing: type[Thing]) -> Thing:
-        """Returns resolved type with deps."""
+from server import implemented
 
 
 class HasContainer:
-    """Base class for all parts that use ``resolve()`` function."""
+    """
+    Base class for all parts that use ``resolve()`` function.
 
-    #: Speed up the resolution, do not import module twice.
-    _resolved: ClassVar[types.ModuleType | None] = None
+    Must be the first base class.
+    """
+
+    __slots__ = ('_container',)
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Create container with dependencies for this class."""
+        super().__init__(*args, **kwargs)
+        self._container = implemented.populate_dependencies(punq.Container())
 
     @final
-    @classmethod
-    def _get_resolve(cls) -> Resolve:
-        if cls._resolved is None:
-            cls._resolved = importlib.import_module('server.implemented')
-        return cls._resolved.resolve  # type: ignore[no-any-return]
-
-    @final
-    def _resolve[Thing](self, thing: type[Thing]) -> Thing:
-        return self._get_resolve()(thing)
+    def resolve[Thing](self, thing: type[Thing]) -> Thing:
+        """Resolve a dependency."""
+        return self._container.resolve(thing)  # type: ignore[no-any-return]
