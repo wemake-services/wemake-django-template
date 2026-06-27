a0b2c4521913a419361e6d90633951122e246c8e
Alexander Kondratev <101856129+kondratevdev@users.noreply.github.com>
2026-06-27 11:50:55 +0500

    Update URL paths for blog post API endpoints (#3066)

--- {{cookiecutter.project_name}}/server/apps/main/api/urls.py
+++ {{cookiecutter.project_name}}/server/apps/main/api/urls.py
@@ -5,6 +5,6 @@ from server.apps.main.api import views
 app_name = 'main'
 
 urlpatterns = [
-    path('users/', views.BlogPostCreate.as_view(), name='blog_post_create'),
-    path('users/<int:id>', views.BlogPostGet.as_view(), name='blog_post_get'),
+    path('posts/', views.BlogPostCreate.as_view(), name='blog_post_create'),
+    path('posts/<int:id>', views.BlogPostGet.as_view(), name='blog_post_get'),
 ]
