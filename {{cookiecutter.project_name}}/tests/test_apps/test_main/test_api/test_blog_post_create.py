7ea150796622e5d74784610b2263cac00ec7e178
Maxim Golovin <mail@golovin.dev>
2026-07-11 05:57:25 +0500

    Add inline-snapshot and inline-snapshot-django. (#3086)
    * feat: add inline-snapshot and inline-snapshot-django.
    
    * feat: add inline-snapshot and inline-snapshot-django.

--- {{cookiecutter.project_name}}/tests/test_apps/test_main/test_api/test_blog_post_create.py
+++ {{cookiecutter.project_name}}/tests/test_apps/test_main/test_api/test_blog_post_create.py
@@ -5,6 +5,8 @@ import msgspec
 import pytest
 from django.urls import reverse
 from dmr.test import DMRClient
+from inline_snapshot import snapshot
+from inline_snapshot_django import snapshot_queries
 from polyfactory.factories.msgspec_factory import MsgspecFactory
 
 from server.apps.main.logic.value_objects import (
@@ -22,11 +24,15 @@ class _BlogPostCreateFactory(MsgspecFactory[BlogPostCreatePayload]):
 @pytest.mark.django_db
 def test_blog_post_create(dmr_client: DMRClient) -> None:
     """Ensures that blog posts can be created."""
-    response = dmr_client.post(
-        reverse('api:main:blog_post_create'),
-        data=msgspec.to_builtins(_BlogPostCreateFactory.build()),
-    )
+    with snapshot_queries() as snap:
+        response = dmr_client.post(
+            reverse('api:main:blog_post_create'),
+            data=msgspec.to_builtins(_BlogPostCreateFactory.build()),
+        )
 
     assert response.status_code == HTTPStatus.CREATED
+    assert snap == snapshot([
+        'INSERT INTO main_blogpost (...) VALUES (...) RETURNING ...',
+    ])
     blog_post = msgspec.convert(response.json(), type=BlogPostFullPayload)
     BlogPost.objects.get(pk=blog_post.id)
