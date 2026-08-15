e6e55f77ddff4d12e9389da5b1e1afe4ed0911af
Maxim Golovin <mail@golovin.dev>
2026-08-15 17:10:59 +0500

    Update urls.

--- {{cookiecutter.project_name}}/server/apps/main/api/urls.py
+++ {{cookiecutter.project_name}}/server/apps/main/api/urls.py
@@ -1,10 +1,15 @@
-from dmr.routing import path
+from dmr.routing import Router, path
 
 from server.apps.main.api import views
 
-app_name = 'main'
-
-urlpatterns = [
-    path('posts/', views.BlogPostCreate.as_view(), name='blog_post_create'),
-    path('posts/<int:id>', views.BlogPostGet.as_view(), name='blog_post_get'),
-]
+router = Router(
+    'user/',
+    [
+        path('posts/', views.BlogPostCreate.as_view(), name='blog_post_create'),
+        path(
+            'posts/<int:id>',
+            views.BlogPostGet.as_view(),
+            name='blog_post_get',
+        ),
+    ],
+)
